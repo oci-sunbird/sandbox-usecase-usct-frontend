75baa9845a480d096fbfc4e483d1cf2ba69eea2e
pearu.sarv <pearu.sarv@twn.ee>
2023-07-05 11:22:34 +0300

    Case list tooltips

--- src/routes/usct/case-list/CaseList.tsx
+++ src/routes/usct/case-list/CaseList.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as MoreIcon } from "@assets/icons/more-horizontal.svg";
+import { ReactComponent as MoreIcon } from '@assets/icons/more-horizontal.svg';
 import {
   Button,
   Flex,
@@ -12,33 +12,34 @@ import {
   Th,
   Thead,
   Tr,
-} from "@chakra-ui/react";
-import Pagination from "@ui/Pagination/Pagination";
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { colors } from "../../../chakra-overrides/colors";
+} from '@chakra-ui/react';
+import Pagination from '@ui/Pagination/Pagination';
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { colors } from '../../../chakra-overrides/colors';
 import {
   ActiveBuildingBlockContext,
   EUserType,
   SimulationContext,
-} from "../USCT";
-import { BUILDING_BLOCK } from "../utils";
+} from '../USCT';
+import { BUILDING_BLOCK } from '../utils';
+import Tooltip from '@ui/Tooltip/Tooltip';
 
 export default function CaseList() {
   const { state, dispatch } = useContext(SimulationContext);
 
   useEffect(() => {
     dispatch({
-      type: "SET_ALL",
+      type: 'SET_ALL',
       ...state,
       userType: EUserType.CITIZEN_SERVANT,
       description: {
-        title: "PHASE 3 - PAYMENT",
-        subtitle: "CIVIL SERVANT REVIEWS THE ACTIVE CASE",
+        title: 'PHASE 3 - PAYMENT',
+        subtitle: 'CIVIL SERVANT REVIEWS THE ACTIVE CASE',
       },
       userAuthorized: true,
-      previousStep: "../case-management?state=done",
-      nextStep: "../review-case/2895379235",
+      previousStep: '../case-management?state=done',
+      nextStep: '../review-case/2895379235',
     });
   }, []);
 
@@ -83,8 +84,64 @@ export default function CaseList() {
             </Text>
           </Flex>
         </Flex>
-        <Flex direction="column" gap="20px">
-          <TableContainer>
+        <Tooltip letter="A" letterPosition="top">
+          <Flex direction="column" gap="20px">
+            <TableContainer>
+              <Table variant="simple">
+                <Thead
+                  backgroundColor={colors.secondary[800]}
+                  color={colors.secondary[0]}
+                >
+                  <Tr>
+                    <Th color={colors.secondary[0]}>#</Th>
+                    <Th color={colors.secondary[0]}>Topic</Th>
+                    <Th color={colors.secondary[0]}>Case Created</Th>
+                    <Th color={colors.secondary[0]}>Case Closed</Th>
+                    <Th color={colors.secondary[0]}>Status</Th>
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>37793946215</Td>
+                    <Td>5</Td>
+                    <Td>High Priority</Td>
+                    <Td>Yesterday</Td>
+                    <Td>Action Required</Td>
+                    <Td>
+                      <MoreIcon />
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+
+            <Flex justifyContent="flex-end">
+              <Button
+                as={Link}
+                to="../review-case/2895379235"
+                colorScheme="admin"
+              >
+                Review Next Case
+              </Button>
+            </Flex>
+          </Flex>
+        </Tooltip>
+      </Flex>
+      <Flex gap="20px" direction="column">
+        <Heading>Case List</Heading>
+        <Flex justifyContent="space-between">
+          <Flex gap="8px" flexShrink="0" alignItems="center">
+            <Text fontSize="12" fontWeight="bold">
+              211
+            </Text>
+            <Text color="secondary.700" fontSize="12">
+              Active Candidates
+            </Text>
+          </Flex>
+        </Flex>
+        <Tooltip letter="B" letterPosition="top">
+          <TableContainer mb="20px">
             <Table variant="simple">
               <Thead
                 backgroundColor={colors.secondary[800]}
@@ -102,10 +159,14 @@ export default function CaseList() {
               <Tbody>
                 <Tr>
                   <Td>37793946215</Td>
-                  <Td>5</Td>
+                  <Td>(unassigned)</Td>
                   <Td>High Priority</Td>
                   <Td>Yesterday</Td>
-                  <Td>Action Required</Td>
+                  <Td>
+                    <Tag width="140px" justifyContent="center">
+                      Pending
+                    </Tag>
+                  </Td>
                   <Td>
                     <MoreIcon />
                   </Td>
@@ -114,66 +175,11 @@ export default function CaseList() {
             </Table>
           </TableContainer>
 
-          <Flex justifyContent="flex-end">
-            <Button
-              as={Link}
-              to="../review-case/2895379235"
-              colorScheme="admin"
-            >
-              Review Next Case
-            </Button>
+          <Flex justifyContent="space-between">
+            <Pagination />
+            <Button isDisabled>Request to Assign New Candidate</Button>
           </Flex>
-        </Flex>
-      </Flex>
-      <Flex gap="20px" direction="column">
-        <Heading>Closed Cases</Heading>
-        <Flex justifyContent="space-between">
-          <Flex gap="8px" flexShrink="0" alignItems="center">
-            <Text fontSize="12" fontWeight="bold">
-              211
-            </Text>
-            <Text color="secondary.700" fontSize="12">
-              CLOSED CASES
-            </Text>
-          </Flex>
-        </Flex>
-        <TableContainer>
-          <Table variant="simple">
-            <Thead
-              backgroundColor={colors.secondary[800]}
-              color={colors.secondary[0]}
-            >
-              <Tr>
-                <Th color={colors.secondary[0]}>#</Th>
-                <Th color={colors.secondary[0]}>Topic</Th>
-                <Th color={colors.secondary[0]}>Case Created</Th>
-                <Th color={colors.secondary[0]}>Case Closed</Th>
-                <Th color={colors.secondary[0]}>Status</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>37793946215</Td>
-                <Td>(unassigned)</Td>
-                <Td>High Priority</Td>
-                <Td>Yesterday</Td>
-                <Td>
-                  <Tag width="140px" justifyContent="center">
-                    Pending
-                  </Tag>
-                </Td>
-                <Td>
-                  <MoreIcon />
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-
-        <Flex justifyContent="flex-end">
-          <Pagination />
-        </Flex>
+        </Tooltip>
       </Flex>
     </Flex>
   );
