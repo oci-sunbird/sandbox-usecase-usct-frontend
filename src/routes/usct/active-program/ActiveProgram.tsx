deaa366fb2fafa8e86ea004f65b8ebd9cdd28953
pearu.sarv <pearu.sarv@twn.ee>
2023-07-05 14:23:35 +0300

    Tooltips

--- src/routes/usct/active-program/ActiveProgram.tsx
+++ src/routes/usct/active-program/ActiveProgram.tsx
@@ -1,5 +1,5 @@
-import { ReactComponent as BanknoteIcon } from "@assets/icons/banknote.svg";
-import { ReactComponent as MaybeCircleIcon } from "@assets/icons/maybe-circle.svg";
+import { ReactComponent as BanknoteIcon } from '@assets/icons/banknote.svg';
+import { ReactComponent as MaybeCircleIcon } from '@assets/icons/maybe-circle.svg';
 import {
   Box,
   Button,
@@ -19,38 +19,39 @@ import {
   Th,
   Thead,
   Tr,
-} from "@chakra-ui/react";
-import FakeLoader from "@ui/FakeLoader/FakeLoader";
-import { useContext, useEffect } from "react";
-import { Link, useNavigation, useSearchParams } from "react-router-dom";
+} from '@chakra-ui/react';
+import FakeLoader from '@ui/FakeLoader/FakeLoader';
+import { useContext, useEffect } from 'react';
+import { Link, useNavigation, useSearchParams } from 'react-router-dom';
 import {
   ActiveBuildingBlockContext,
   EUserType,
   SimulationContext,
-} from "../USCT";
-import { BUILDING_BLOCK } from "../utils";
+} from '../USCT';
+import { BUILDING_BLOCK } from '../utils';
+import Tooltip from '@ui/Tooltip/Tooltip';
 
 const getConfig = (state: string | null) => {
   switch (state) {
-    case "done":
+    case 'done':
       return {
         description: {
-          title: "PHASE 3 - PAYMENT",
+          title: 'PHASE 3 - PAYMENT',
           subtitle:
-            "CITIZEN REVIEWS THEIR PROGRAM AND STARTS A NEW CONVERSATION",
+            'CITIZEN REVIEWS THEIR PROGRAM AND STARTS A NEW CONVERSATION',
         },
-        nextStep: "../conversation/300",
-        previousStep: "../review-case/2895379235",
+        nextStep: '../conversation/300',
+        previousStep: '../review-case/2895379235',
       };
     default:
       return {
         description: {
-          title: "PHASE 3 - PAYMENT",
+          title: 'PHASE 3 - PAYMENT',
           subtitle:
-            "CITIZEN REVIEWS THEIR PROGRAM AND STARTS A NEW CONVERSATION",
+            'CITIZEN REVIEWS THEIR PROGRAM AND STARTS A NEW CONVERSATION',
         },
-        nextStep: "../new-conversation",
-        previousStep: "../review-candidate/2895379235?state=scheduling",
+        nextStep: '../new-conversation',
+        previousStep: '../review-candidate/2895379235?state=scheduling',
       };
   }
 };
@@ -61,11 +62,11 @@ export default function ActiveProgram() {
   const navigation = useNavigation();
   useEffect(() => {
     dispatch({
-      type: "SET_ALL",
+      type: 'SET_ALL',
       ...state,
       userType: EUserType.CITIZEN,
       userAuthorized: true,
-      ...getConfig(searchParams.get("state")),
+      ...getConfig(searchParams.get('state')),
     });
   }, [location]);
 
@@ -87,7 +88,7 @@ export default function ActiveProgram() {
 
   return (
     <FakeLoader label="CHANGING PERSPECTIVE TO APPLICANT">
-      <Flex direction="column" gap="20px" width="100%">
+      <Flex direction="column" gap="40px" width="100%">
         <Flex direction="column">
           <Heading fontSize="36px">Active Program:</Heading>
           <Heading fontSize="24px">Unconditional Social Cash Transfer</Heading>
@@ -100,172 +101,184 @@ export default function ActiveProgram() {
           </Text>
         </Flex>
 
-        <Flex
-          background="theme.light"
-          direction="column"
-          gap="20px"
-          padding="20px 60px"
-          mx="-60px"
-        >
-          <Heading>Conversations</Heading>
-          <Text>Conversations</Text>
+        <Box background="theme.light" px="60px" mx="-60px">
+          <Tooltip
+            py="20px"
+            letter="A"
+            display="flex"
+            flexDirection="column"
+            gap="20px"
+          >
+            <Heading>Conversations</Heading>
+            <Text>Conversations</Text>
 
-          <Tabs isFitted>
-            <TabList>
-              <Tab>My Active Conversations (1)</Tab>
-              <Tab>Closed Conversations (0)</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel
-                gap="20px"
-                display="flex"
-                flexDirection="column"
-                padding={0}
-              >
-                <TableContainer>
-                  <Table background="white">
-                    <Thead>
-                      <Tr>
-                        <Th>Date</Th>
-                        <Th>Topic</Th>
-                        <Th>Attachment</Th>
-                        <Th>Status</Th>
-                        <Th>#</Th>
-                      </Tr>
-                    </Thead>
-                    <Tbody>
-                      <Tr>
-                        <Td>01.01.2023</Td>
-                        <Td>Package Information</Td>
-                        <Td>USCT - Monthly Benefit Package</Td>
-                        <Td>
-                          <Flex gap="10px" alignItems="center">
-                            <MaybeCircleIcon /> In Progress
-                          </Flex>
-                        </Td>
-                        <Td>3779394</Td>
-                      </Tr>
-                    </Tbody>
-                  </Table>
-                </TableContainer>
-              </TabPanel>
-              <TabPanel></TabPanel>
-            </TabPanels>
-          </Tabs>
+            <Tabs isFitted>
+              <TabList>
+                <Tab>My Active Conversations (1)</Tab>
+                <Tab>Closed Conversations (0)</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel
+                  gap="20px"
+                  display="flex"
+                  flexDirection="column"
+                  padding={0}
+                >
+                  <TableContainer>
+                    <Table background="white">
+                      <Thead>
+                        <Tr>
+                          <Th>Date</Th>
+                          <Th>Topic</Th>
+                          <Th>Attachment</Th>
+                          <Th>Status</Th>
+                          <Th>#</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        <Tr>
+                          <Td>01.01.2023</Td>
+                          <Td>Package Information</Td>
+                          <Td>USCT - Monthly Benefit Package</Td>
+                          <Td>
+                            <Flex gap="10px" alignItems="center">
+                              <MaybeCircleIcon /> In Progress
+                            </Flex>
+                          </Td>
+                          <Td>3779394</Td>
+                        </Tr>
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </TabPanel>
+                <TabPanel></TabPanel>
+              </TabPanels>
+            </Tabs>
 
-          <Button as={Link} to={state.nextStep} colorScheme="citizen" ml="auto">
-            Review Conversation
-          </Button>
-        </Flex>
+            <Button
+              as={Link}
+              to={state.nextStep}
+              colorScheme="citizen"
+              ml="auto"
+            >
+              Review Conversation
+            </Button>
+          </Tooltip>
+        </Box>
 
-        <Flex direction="column" gap="20px">
-          <Flex direction="column">
-            <Heading fontSize="18px">Assigned Benefit Package:</Heading>
-            <Text>Monthly Benefit Package</Text>
+        <Tooltip letter="B">
+          <Flex direction="column" gap="20px">
+            <Flex direction="column">
+              <Heading fontSize="18px">Assigned Benefit Package:</Heading>
+              <Text>Monthly Benefit Package</Text>
+            </Flex>
+            <SimpleGrid columns={2} spacing="20px">
+              <Flex alignItems="center" gap="20px">
+                <Flex
+                  borderRadius="8px"
+                  alignItems="center"
+                  justifyContent="center"
+                  flexShrink="0"
+                  w="64px"
+                  h="64px"
+                  border="2px dashed black"
+                >
+                  <BanknoteIcon
+                    width="34px"
+                    height="34px"
+                    color="var(--chakra-colors-secondary-1000)"
+                  />
+                </Flex>
+                <Flex direction="column" gap="8px">
+                  <Text>
+                    Monthly cash help for you and your family without any
+                    additional conditions.
+                  </Text>
+                </Flex>
+              </Flex>
+              <Flex alignItems="center" gap="20px">
+                <Flex
+                  borderRadius="8px"
+                  alignItems="center"
+                  justifyContent="center"
+                  flexShrink="0"
+                  w="64px"
+                  h="64px"
+                  border="2px dashed black"
+                >
+                  <BanknoteIcon
+                    width="34px"
+                    height="34px"
+                    color="var(--chakra-colors-secondary-1000)"
+                  />
+                </Flex>
+                <Flex direction="column" gap="4px">
+                  <Text>
+                    Career assessment, online consultation and job referrals.
+                  </Text>
+                </Flex>
+              </Flex>
+            </SimpleGrid>
+            <SimpleGrid columns={2} spacing="20px">
+              <Box>
+                <Text fontWeight="600">Payment Amount</Text>
+                <Text>1 234.00 at the beginning of each month</Text>
+              </Box>
+              <Box>
+                <Text fontWeight="600">Consultation Date</Text>
+                <Text>Please select a date for your consultation</Text>
+              </Box>
+              <Box>
+                <Text fontWeight="600">Duration</Text>
+                <Text>01.01.2023 - 01.06.2023 (6 Months)</Text>
+              </Box>
+              <Box>
+                <Text fontWeight="600">Location</Text>
+                <Text>Online - Link will be shared ...</Text>
+              </Box>
+              <Box>
+                <Text fontWeight="600">Payment Method</Text>
+                <Text>Bank Payment</Text>
+              </Box>
+            </SimpleGrid>
           </Flex>
-          <SimpleGrid columns={2} spacing="20px">
-            <Flex alignItems="center" gap="20px">
-              <Flex
-                borderRadius="8px"
-                alignItems="center"
-                justifyContent="center"
-                flexShrink="0"
-                w="64px"
-                h="64px"
-                border="2px dashed black"
-              >
-                <BanknoteIcon
-                  width="34px"
-                  height="34px"
-                  color="var(--chakra-colors-secondary-1000)"
-                />
-              </Flex>
-              <Flex direction="column" gap="8px">
-                <Text>
-                  Monthly cash help for you and your family without any
-                  additional conditions.
-                </Text>
-              </Flex>
-            </Flex>
-            <Flex alignItems="center" gap="20px">
-              <Flex
-                borderRadius="8px"
-                alignItems="center"
-                justifyContent="center"
-                flexShrink="0"
-                w="64px"
-                h="64px"
-                border="2px dashed black"
-              >
-                <BanknoteIcon
-                  width="34px"
-                  height="34px"
-                  color="var(--chakra-colors-secondary-1000)"
-                />
-              </Flex>
-              <Flex direction="column" gap="4px">
-                <Text>
-                  Career assessment, online consultation and job referrals.
-                </Text>
-              </Flex>
-            </Flex>
-          </SimpleGrid>
-          <SimpleGrid columns={2} spacing="20px">
-            <Box>
-              <Text fontWeight="600">Payment Amount</Text>
-              <Text>1 234.00 at the beginning of each month</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="600">Consultation Date</Text>
-              <Text>Please select a date for your consultation</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="600">Duration</Text>
-              <Text>01.01.2023 - 01.06.2023 (6 Months)</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="600">Location</Text>
-              <Text>Online - Link will be shared ...</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="600">Payment Method</Text>
-              <Text>Bank Payment</Text>
-            </Box>
-          </SimpleGrid>
-        </Flex>
-        <Flex direction="column" gap="20px">
-          <Heading fontSize="18px">Package payment</Heading>
-          <TableContainer>
-            <Table>
-              <Thead>
-                <Tr>
-                  <Th>Date</Th>
-                  <Th>Method</Th>
-                  <Th>Amount</Th>
-                  <Th>Payment Details</Th>
-                  <Th>Status</Th>
-                  <Th>Invoice</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>01.01.2023</Td>
-                  <Td>Bank Payment</Td>
-                  <Td>1 234.00</Td>
-                  <Td>USCT - Monthly Benefit Package</Td>
-                  <Td>
-                    <Flex gap="10px" alignItems="center">
-                      <MaybeCircleIcon /> In Progress
-                    </Flex>
-                  </Td>
-                  <Td>#123456789</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
+        </Tooltip>
 
-          <Text fontWeight="600">Next Payment: 02.01.2023</Text>
-        </Flex>
+        <Tooltip letter="C">
+          <Flex direction="column" gap="20px">
+            <Heading fontSize="18px">Payment History</Heading>
+            <TableContainer>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th>Date</Th>
+                    <Th>Method</Th>
+                    <Th>Amount</Th>
+                    <Th>Payment Details</Th>
+                    <Th>Status</Th>
+                    <Th>Invoice</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>01.01.2023</Td>
+                    <Td>Bank Payment</Td>
+                    <Td>1 234.00</Td>
+                    <Td>USCT - Monthly Benefit Package</Td>
+                    <Td>
+                      <Flex gap="10px" alignItems="center">
+                        <MaybeCircleIcon /> In Progress
+                      </Flex>
+                    </Td>
+                    <Td>#123456789</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+
+            <Text fontWeight="600">Next Payment: 02.01.2023</Text>
+          </Flex>
+        </Tooltip>
       </Flex>
     </FakeLoader>
   );
--- src/routes/usct/info/Info.tsx
+++ src/routes/usct/info/Info.tsx
@@ -9,35 +9,37 @@ import {
   Flex,
   Heading,
   Text,
-} from "@chakra-ui/react";
-import FakeLoader from "@ui/FakeLoader/FakeLoader";
-import { useContext, useEffect } from "react";
-import { Link, useNavigation, useSearchParams } from "react-router-dom";
+} from '@chakra-ui/react';
+import FakeLoader from '@ui/FakeLoader/FakeLoader';
+import { useContext, useEffect } from 'react';
+import { Link, useNavigation, useSearchParams } from 'react-router-dom';
 import {
   ActiveBuildingBlockContext,
   EUserType,
   SimulationContext,
-} from "../USCT";
-import { BUILDING_BLOCK } from "../utils";
+} from '../USCT';
+import { BUILDING_BLOCK } from '../utils';
+import { ReactComponent as FileWarningIcon } from '@assets/icons/file-warning.svg';
+import Tooltip from '@ui/Tooltip/Tooltip';
 
 const getConfig = (done: boolean) => {
   if (done) {
     return {
       description: {
-        title: "PHASE 2 - ENROLMENT",
-        subtitle: "CITIZEN REVIEWS ELIGIBILITY STATUS",
+        title: 'PHASE 2 - ENROLMENT',
+        subtitle: 'CITIZEN REVIEWS ELIGIBILITY STATUS',
       },
-      previousStep: "../review-candidate/2895379235?state=done",
-      nextStep: "../enrolment",
+      previousStep: '../review-candidate/2895379235?state=done',
+      nextStep: '../enrolment',
     };
   } else {
     return {
       description: {
-        title: "PHASE 1 - ELIGIBILITY",
-        subtitle: "CITIZEN CHECKS THE BENEFICIARY PROGRAM",
+        title: 'PHASE 1 - ELIGIBILITY',
+        subtitle: 'CITIZEN CHECKS THE BENEFICIARY PROGRAM',
       },
-      previousStep: "../authorise-citizen",
-      nextStep: "../personal",
+      previousStep: '../authorise-citizen',
+      nextStep: '../personal',
     };
   }
 };
@@ -49,10 +51,10 @@ export default function Info() {
 
   useEffect(() => {
     dispatch({
-      type: "SET_ALL",
+      type: 'SET_ALL',
       ...state,
       userType: EUserType.CITIZEN,
-      ...getConfig(!!searchParams.get("done")),
+      ...getConfig(!!searchParams.get('done')),
       userAuthorized: true,
     });
   }, [location]);
@@ -76,12 +78,12 @@ export default function Info() {
   return (
     <FakeLoader
       label="CHANGING PERSPECTIVE TO APPLICANT"
-      override={!!searchParams.get("done")}
+      override={!!searchParams.get('done')}
     >
       <Flex
         mt="128px"
-        gap={{ sm: "20px", lg: "125px" }}
-        direction={{ sm: "column", lg: "row" }}
+        gap={{ sm: '20px', lg: '125px' }}
+        direction={{ sm: 'column', lg: 'row' }}
       >
         <Box>
           <Box marginBottom="40px">
@@ -100,7 +102,7 @@ export default function Info() {
           <Accordion>
             <AccordionItem>
               <Heading variant="h2" size="md">
-                <AccordionButton style={{ font: "inherit" }} px="0">
+                <AccordionButton style={{ font: 'inherit' }} px="0">
                   <Box as="span" flex="1" textAlign="left">
                     How does it work?
                   </Box>
@@ -116,7 +118,7 @@ export default function Info() {
             </AccordionItem>
             <AccordionItem>
               <Heading variant="h2" size="md">
-                <AccordionButton style={{ font: "inherit" }} px="0">
+                <AccordionButton style={{ font: 'inherit' }} px="0">
                   <Box as="span" flex="1" textAlign="left">
                     Eligibility and Requirements
                   </Box>
@@ -132,7 +134,7 @@ export default function Info() {
             </AccordionItem>
             <AccordionItem>
               <Heading variant="h2" size="md">
-                <AccordionButton style={{ font: "inherit" }} px="0">
+                <AccordionButton style={{ font: 'inherit' }} px="0">
                   <Box as="span" flex="1" textAlign="left">
                     Similar Programs
                   </Box>
@@ -150,55 +152,54 @@ export default function Info() {
         </Box>
         <Flex
           direction="column"
-          gap="16px"
-          width={{ lg: "25%" }}
+          gap="20px"
+          width={{ lg: '25%' }}
           flexShrink="0"
         >
-          <Flex gap="8px">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M14.5 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V7.5L14.5 2Z"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M12 9V13"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M12 17H12.01"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            Your Eligibility
-          </Flex>
-          {searchParams.get("done") ? (
-            <>
-              <Text>
-                <strong>You are eligible</strong> for Unconditional Social Cash
-                Transfer Program!
-              </Text>
-              <Text>
-                Please check your enrolment status from enrolment page.
-              </Text>
-              <Button as={Link} to="../enrolment" colorScheme="citizen">
-                Enrolment
-              </Button>
+          <Tooltip
+            letter="A"
+            letterPosition="right-center"
+            display="flex"
+            flexDirection="column"
+            gap="16px"
+          >
+            <Flex gap="8px">
+              <FileWarningIcon />
+              Your Eligibility
+            </Flex>
+            {searchParams.get('done') ? (
+              <>
+                <Text>
+                  <strong>You are eligible</strong> for Unconditional Social
+                  Cash Transfer Program!
+                </Text>
+                <Text>
+                  Please check your enrolment status from enrolment page.
+                </Text>
+                <Button as={Link} to="../enrolment" colorScheme="citizen">
+                  Enrolment
+                </Button>
+              </>
+            ) : (
+              <>
+                <Text>
+                  There is <strong>not enough information</strong> to decide
+                  your eligibility for this program
+                </Text>
+                <Text>
+                  Please review your information and provide missing information
+                  if needed.
+                </Text>
+                <Button colorScheme="citizen" to="../personal" as={Link}>
+                  Review
+                </Button>
+              </>
+            )}
+          </Tooltip>
+          {!!searchParams.get('done') && (
+            <Tooltip letter="B">
               <Button
+                w="100%"
                 colorScheme="citizen"
                 variant="outline"
                 as={Link}
@@ -206,21 +207,7 @@ export default function Info() {
               >
                 My Information
               </Button>
-            </>
-          ) : (
-            <>
-              <Text>
-                There is <strong>not enough information</strong> to decide your
-                eligibility for this program
-              </Text>
-              <Text>
-                Please review your information and provide missing information
-                if needed.
-              </Text>
-              <Button colorScheme="citizen" to="../personal" as={Link}>
-                Review
-              </Button>
-            </>
+            </Tooltip>
           )}
         </Flex>
       </Flex>
